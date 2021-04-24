b92b1c62a27cb90c7c91e0520b12d0e0eb16a48f
Golbarg200 <pepi_krastev@abv.bg>
2021-04-24 15:12:36 +0300

    Added Builder and Singleton

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -6,7 +6,22 @@ namespace Builder
     {
         static void Main(string[] args)
         {
-            
+            var director = new Director();
+            var builder = new ConcreteBuilder();
+            director.Builder = builder;
+
+            Console.WriteLine("Standard basic product:");
+            director.BuildMinimalViableProduct();
+            Console.WriteLine(builder.GetProduct().ListParts());
+
+            Console.WriteLine("Standard full featured product:");
+            director.BuildFullFeaturedProduct();
+            Console.WriteLine(builder.GetProduct().ListParts());
+
+            Console.WriteLine("Custom product:");
+            builder.BuildPartA();
+            builder.BuildPartC();
+            Console.Write(builder.GetProduct().ListParts());
         }
     }
 }
