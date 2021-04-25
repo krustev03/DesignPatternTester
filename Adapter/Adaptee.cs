c6a93391d00a22921811be4c7f63f67945658fa8
Golbarg200 <pepi_krastev@abv.bg>
2021-04-25 06:10:31 +0300

    Added Singleton and Adapter

---
+++ Adapter/Adaptee.cs
@@ -0,0 +1,10 @@
+﻿namespace Adapter
+{
+    class Adaptee
+    {
+        public string GetSpecificRequest()
+        {
+            return "Specific request.";
+        }
+    }
+}
---
+++ Adapter/Adapter.cs
@@ -0,0 +1,19 @@
+﻿using Adapter.Interfaces;
+
+namespace Adapter
+{
+    class Adapter : ITarget
+    {
+        private readonly Adaptee _adaptee;
+
+        public Adapter(Adaptee adaptee)
+        {
+            this._adaptee = adaptee;
+        }
+
+        public string GetRequest()
+        {
+            return $"This is '{this._adaptee.GetSpecificRequest()}'";
+        }
+    }
+}
