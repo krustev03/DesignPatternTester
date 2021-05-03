dd076c5a55e9049570cc28358a23f2d2c10c7e09
Golbarg200 <pepi_krastev@abv.bg>
2021-05-03 12:23:41 +0300

    Added State & Observer

---
+++ Observer/Interfaces/ISubject.cs
@@ -0,0 +1,11 @@
+﻿namespace Observer.Interfaces
+{
+    public interface ISubject
+    {
+        void Attach(IObserver observer);
+
+        void Detach(IObserver observer);
+
+        void Notify();
+    }
+}
---
+++ State/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace State
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new Context(new ConcreteStateA());
+            context.Request1();
+            context.Request2();
+        }
+    }
+}
